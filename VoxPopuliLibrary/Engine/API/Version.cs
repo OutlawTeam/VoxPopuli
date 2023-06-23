075adf5f385f1eb0dbe499d5565bfbf8c4482edf
Bambou72 <pfeiffer.florian72@gmail.com>
2023-06-23 15:37:51 +0200

    Last Second Change

--- VoxPopuliLibrary/Engine/API/Version.cs
+++ VoxPopuliLibrary/Engine/API/Version.cs
@@ -2,7 +2,7 @@
 {
     internal static class Version
     {
-        public static string EngineVersion = "0.1.1";
+        public static string EngineVersion = "0.1.1.patch1";
         public static string APIVersion = "0.1.1";
     }
 }
